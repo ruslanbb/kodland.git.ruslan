c000a04eace677106bede758a583d384bfd64745
ruslanbb <crow_1010@mail.ru>
2024-07-25 19:51:36 +0500

    Create pythonpro_main.py

---
+++ pythonpro_main.py
@@ -0,0 +1,33 @@
+import random
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def pasw(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run("")
