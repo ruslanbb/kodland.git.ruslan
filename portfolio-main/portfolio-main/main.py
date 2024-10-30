a3a57bdfc3ff69d5d1d3f25f2ba5148aa0952007
ruslanbb <crow_1010@mail.ru>
2024-10-30 20:17:47 +0500

    Add files via upload

---
+++ portfolio-main/portfolio-main/main.py
@@ -0,0 +1,40 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+    return render_template('index.html', 
+                           button_python=button_python,
+                           button_discord=button_discord,
+                           button_html=button_html,
+                           button_db=button_db)
+
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    email = request.form.get('email')
+    text = request.form.get('text')
+    print(email, text)
+    
+    with open('messages.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{email}\n{text}\n\n')
+
+    return render_template('index.html')
+                           
+
+if __name__ == "__main__":
+    app.run(debug=True)
